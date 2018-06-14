99785af95b187510d3c56d2155d045b601847006
Gabriela <gabrielasosaft@gmail.com>
2018-06-14 16:49:42 +0200

    ejrcicios

--- proyectoJquery/js/Mijs.js
+++ proyectoJquery/js/Mijs.js
@@ -44,5 +44,54 @@ $('#calendario').datepicker();
 //------ Tabs
 $('#tabs').tabs(); 
 
+//-------------------
+//
+var target;
+var triggers = $('ul.triggers li');
+var images = $('ul.images li');
+var lastElem = triggers.length-1;
+var mask = $('.mask ul.images');
+var imgWidth = images.width();
 
+triggers.first().addClass('active');
+mask.css('width', imgWidth*(lastElem+1) +'px');
+
+function sliderResponse(target) {
+    mask.stop(true,false).animate({'left':'-'+ imgWidth*target +'px'},300);
+    triggers.removeClass('active').eq(target).addClass('active');
+}
+
+triggers.click(function() {
+    if ( !$(this).hasClass('active') ) {
+        target = $(this).index();
+        sliderResponse(target);
+        resetTiming();
+    }
+});
+
+$('.next').click(function() {
+    target = $('ul.triggers li.active').index();
+    target === lastElem ? target = 0 : target = target+1;
+    sliderResponse(target);
+    resetTiming();
+});
+$('.prev').click(function() {
+    target = $('ul.triggers li.active').index();
+    lastElem = triggers.length-1;
+    target === 0 ? target = lastElem : target = target-1;
+    sliderResponse(target);
+    resetTiming();
+});
+
+function sliderTiming() {
+    target = $('ul.triggers li.active').index();
+    target === lastElem ? target = 0 : target = target+1;
+    sliderResponse(target);
+}
+
+var timingRun = setInterval(function() { sliderTiming(); },5000);
+function resetTiming() {
+    clearInterval(timingRun);
+    timingRun = setInterval(function() { sliderTiming(); },5000);
+}
 })//fin document.ready
